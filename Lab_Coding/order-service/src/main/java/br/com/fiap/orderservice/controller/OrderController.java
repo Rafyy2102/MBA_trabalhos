97017b382322f9bf276471c22fe7e5ea39b1f0dd
Rafyy2102 (Rafaela dos Santos Silva) <rafpromais@yahoo.com.br>
2021-10-02 15:17:59 -0300

    Atividade Coding

--- Lab_Coding/order-service/src/main/java/br/com/fiap/orderservice/controller/OrderController.java
+++ Lab_Coding/order-service/src/main/java/br/com/fiap/orderservice/controller/OrderController.java
@@ -1,6 +1,8 @@
 package br.com.fiap.orderservice.controller;
 
-import br.com.fiap.orderservice.entity.Order;
+import br.com.fiap.orderservice.dto.OrderDTO;
+import br.com.fiap.orderservice.exeption.EntityNotFoundException;
+import br.com.fiap.orderservice.exeption.InvalidoRegistroException;
 import br.com.fiap.orderservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,8 +14,29 @@ public class OrderController {
     @Autowired
     private OrderService service;
 
-    @PostMapping("/order")
-    public ResponseEntity<Order>getSave(@RequestBody Order order){
+    @GetMapping("/findById/{idOrder}")
+    public ResponseEntity findById(@PathVariable("idOrder") int idOrder) throws EntityNotFoundException {
+        OrderDTO order = service.findById(idOrder);
+        return new ResponseEntity<>(order, HttpStatus.OK);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<String> save(@RequestBody OrderDTO order) {
+        service.save(order);
+        return new ResponseEntity<>("http://localhost:8080/findById/" + (service.getCurrentId() - 1),
+                HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("/update/{idOrder}")
+    public ResponseEntity<OrderDTO> update(@PathVariable("idOrder") int idOrder, @RequestBody OrderDTO order)
+            throws EntityNotFoundException, InvalidoRegistroException {
+        service.update(idOrder, order);
         return new ResponseEntity<>(order, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/delete/{idOrder}")
+    public ResponseEntity delete(@PathVariable("idOrder") int idOrder) {
+        OrderDTO order = service.delete(idOrder);
+        return new ResponseEntity<>(order, HttpStatus.OK);
     }
 }
