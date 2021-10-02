e8fa33149f9210bc1a9914fc39e7313905aa8f6f
Rafyy2102 (Rafaela dos Santos Silva) <rafpromais@yahoo.com.br>
2021-10-02 13:18:04 -0300

    Lab Cloud e DevOps

---
+++ Lab_Coding/order-service/src/main/java/br/com/fiap/orderservice/controller/OrderController.java
@@ -0,0 +1,19 @@
+package br.com.fiap.orderservice.controller;
+
+import br.com.fiap.orderservice.dto.Order;
+import br.com.fiap.orderservice.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class OrderController {
+    @Autowired
+    private OrderService service;
+
+    @PostMapping("/order")
+    public ResponseEntity<Order>getSave(@RequestBody Order order){
+        return new ResponseEntity<>(order, HttpStatus.ACCEPTED);
+    }
+}
---
+++ Lab_Coding/order-service/src/main/java/br/com/fiap/orderservice/repository/OrderRespository.java
@@ -0,0 +1,8 @@
+package br.com.fiap.orderservice.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OrderRespository {
+
+}
